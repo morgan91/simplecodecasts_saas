5ec65fc1f90c1629f665ceaee81b5b7668fb4454
m3rg8n <morganjrusso@gmail.com>
2015-10-21 00:05:02 +0000

    The Flash Hash

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,16 +3,16 @@ class ContactsController < ApplicationController
         @contact = Contact.new		
     end		
     		
-    def create		
-        @contact = Contact.new(contact_params)		
-        		
-        if @contact.save		
-            redirect_to new_contact_path, notice: "Message sent."		
-        else		
-            redirect_to new_contact_path, notice: "Error Occured."		
-        end		
-    end		
-    		
+ def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+    flash[:success] = 'Message sent.'
+    redirect_to new_contact_path
+  else
+    flash[:danger] = 'Error occured, message has not been sent.'
+    redirect_to new_contact_path
+  end
+end
     		
     private		
         def contact_params		
